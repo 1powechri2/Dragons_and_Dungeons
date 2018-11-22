30e5057c34004ba79222230364d04aed46bb9eac
Chris Powell <chris.ian.powell@gmail.com>
2018-11-21 17:16:30 -0700

    Adds boilerplate for create campaign or create character on user page

--- d_and_d/urls.py
+++ d_and_d/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('sign_up', views.sign_up, name='sign_up'),
     path('login_user', views.login_user, name='login_user'),
     path('user_page', views.user_page, name='user_page'),
-    path('logout_user', views.logout_user, name='logout_user')
+    path('logout_user', views.logout_user, name='logout_user'),
 ]
